4effce859bef841e34f90d366e6f83a76a392bed
Pedro Pablo Carrasco <pedropablocarrascob@gmail.com>
2024-06-12 17:02:27 -0400

    actualización

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import React, {Component} from 'react';
 class App extends Component {
 
   state = {
-    movie: {}
+    movie: {},
+    isFetching: false
   }
-
 
   handleSubmit = (event) => {
     event.preventDefault()
+
+    this.setState( {isFetching: true})
 
     const title = event.target[0].value
     const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=4d5298cf'
     fetch(url + '&t=' + title)
     .then(res => res.json())
-    .then(movie => this.setState({movie}))
+    .then(movie => this.setState({movie, isFetching: false}))
 
   }
 
   render() {
+    const {movie, isFetching } = this.state
     return (
       <div>
         <h1>
@@ -34,11 +37,22 @@ class App extends Component {
               Buscar
             </button>
         </form>
+        { isFetching && (
+          <h2>Cargando...</h2>
+        )}
+        { movie.Title && !isFetching }
+
 
         <div>
           <h1>{this.state.movie.Title}</h1>
           <p>{this.state.movie.Plot}</p>
-          <img src={this.state.movie.Poster} alt='Poster'/>
+          <img 
+          src={movie.Poster} 
+          alt='Poster'
+          style={{
+            width: '150px'
+          }}
+          />
         </div>
       </div>
     );
