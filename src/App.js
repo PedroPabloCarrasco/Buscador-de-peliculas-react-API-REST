98458c552939b8c87aed9eb11aaf36055cf3dd7f
Pedro Pablo Carrasco <pedropablocarrascob@gmail.com>
2024-06-12 14:10:38 -0400

    Integración de la api y busqueda
    Integracion de la api y primeros vistazos del detalle de la pelicula

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ class App extends Component {
     movie: {}
   }
 
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    const title = event.target[0].value
+    const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=4d5298cf'
+    fetch(url + '&t=' + title)
+    .then(res => res.json())
+    .then(movie => this.setState({movie}))
+
+  }
+
   render() {
     return (
       <div>
         <h1>
           Ejemplo Http buscador de peliculas
         </h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input 
             type='text'
             placeholder='Nombre de la pelicula'
@@ -22,6 +34,12 @@ class App extends Component {
               Buscar
             </button>
         </form>
+
+        <div>
+          <h1>{this.state.movie.Title}</h1>
+          <p>{this.state.movie.Plot}</p>
+          <img src={this.state.movie.Poster} alt='Poster'/>
+        </div>
       </div>
     );
   }
