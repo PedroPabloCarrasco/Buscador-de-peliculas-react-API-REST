131b506f83abfe9f992585e570fc401538a2b77f
Pedro Pablo Carrasco <pedropablocarrascob@gmail.com>
2024-06-13 13:12:12 -0400

    Actualización

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-import axios from 'axios'
-
-axios.get()
+import axios from 'axios';
 
 class App extends Component {
 
@@ -10,30 +8,29 @@ class App extends Component {
     isFetching: false
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault()
+  handleSubmit = async (event) => {
+    event.preventDefault();
 
-    this.setState( {isFetching: true})
+    this.setState({ isFetching: true });
 
-    const title = event.target[0].value
-    const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=4d5298cf'
-    //fetch(url + '&t=' + title)
-    axios.get(url, {
-      params:{
-        t: title
-      }
+    const title = event.target[0].value;
+    const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=4d5298cf';
+
+    try {
+      const response = await axios.get(url, {
+        params: {
+          t: title
+        }
+      });
+      this.setState({ movie: response.data, isFetching: false });
+    } catch (error) {
+      console.error('Error fetching the movie data:', error);
+      this.setState({ isFetching: false });
     }
-      
-    )
-    //.then(res => res.json())
-    .then(res => this.setState({
-      movie: res.data,  
-      isFetching: false}))
-
   }
 
   render() {
-    const {movie, isFetching } = this.state
+    const { movie, isFetching } = this.state;
     return (
       <div>
         <h1>
@@ -43,29 +40,27 @@ class App extends Component {
           <input 
             type='text'
             placeholder='Nombre de la pelicula'
-            />
-
-            <button>
-              Buscar
-            </button>
+          />
+          <button>
+            Buscar
+          </button>
         </form>
         { isFetching && (
           <h2>Cargando...</h2>
         )}
-        { movie.Title && !isFetching }
-
-
-        <div>
-          <h1>{this.state.movie.Title}</h1>
-          <p>{this.state.movie.Plot}</p>
-          <img 
-          src={movie.Poster} 
-          alt='Poster'
-          style={{
-            width: '150px'
-          }}
-          />
-        </div>
+        { movie.Title && !isFetching && (
+          <div>
+            <h1>{movie.Title}</h1>
+            <p>{movie.Plot}</p>
+            <img 
+              src={movie.Poster} 
+              alt='Poster'
+              style={{
+                width: '150px'
+              }}
+            />
+          </div>
+        )}
       </div>
     );
   }
